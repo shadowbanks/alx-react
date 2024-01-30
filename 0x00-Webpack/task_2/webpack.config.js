924c2eabaf6e1f6265527d7f09829e813c5d7ce9
WIlliams <akanniwilliams@gmail.com>
2024-01-30 04:49:40 +0100

    Add CSS & Images & index

---
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'production',
+    entry: {
+        main: path.resolve(__dirname, './js/dashboard_main.js'),
+    },
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/i,
+                use: [
+                    'file-loader',
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            bypassOnDebug: true,
+                            disable: true,
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+};
