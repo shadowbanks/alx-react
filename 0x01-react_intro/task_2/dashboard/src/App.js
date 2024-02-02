a3fa39a6698f09525ed8b3280d81750e4d10c032
WIlliams <akanniwilliams@gmail.com>
2024-02-02 18:29:05 +0100

    Add input fields

---
+++ 0x01-react_intro/task_2/dashboard/src/App.js
@@ -0,0 +1,37 @@
+import logo from './holberton-logo.jpg';
+import './App.css';
+import './utils'
+import { Component } from 'react';
+import { getFooterCopy, getFullYear } from './utils';
+
+class App extends Component {
+  onLabelClick = (inpuId) => {
+    const inputElement = document.getElementById(inpuId);
+    if (inputElement) {
+      inputElement.focus();
+    }
+  }
+  render() {
+    return (
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <h1>School dashboard</h1>
+        </header>
+        <div className='App-body'>
+          <p>Login to access the full dashboard</p>
+          <label htmlFor='email' onClick={() => this.onLabelClick('email')}>Email</label>
+          <input id='email' type='email' name='email' />
+          <label htmlFor='password' onClick={() => this.onLabelClick('password')}>Password</label>
+          <input id='password' type='password' name='password' />
+          <input id='ok' type='button' name='ok_button' value='OK' />
+        </div>
+        <div className='App-footer'>
+          <p>Copyright { getFullYear() } - { getFooterCopy() } </p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
