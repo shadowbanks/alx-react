bddfa366f654e313a18c82ef413ca35f2b86711b
WIlliams <akanniwilliams@gmail.com>
2024-02-03 10:15:31 +0100

    Update how close button action is implemented

--- 0x01-react_intro/task_2/dashboard/src/Notifications.js
+++ 0x01-react_intro/task_2/dashboard/src/Notifications.js
@@ -4,11 +4,13 @@ import closeIcon from './close-icon.png';
 import { getLatestNotification } from './utils';
 
 const Notifications = () => {
+    const handleClick = () => {
+        console.log('Close button has been clicked');
+    }
     return (
         <>
             <div style={{ position: 'relative' }} className='Notifications'>
-                <button aria-label='Close'
-                    onClick={ () => console.log('Close button has been clicked') }
+                <button aria-label='Close' onClick={handleClick}
                     style={{ position: 'absolute', top: '0', right: '0', padding: '.3rem',  border: 'none', background: 'none', }}
                 >
                         <img src={closeIcon} alt='Close Icon' style={{ height: '1.3rem', width: '1.3rem'}} />
