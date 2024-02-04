170976b5c94d991c346a1abc51aba4493cf35b5c
WIlliams <akanniwilliams@gmail.com>
2024-02-04 14:04:31 +0100

    Create Tests for util

---
+++ 0x01-react_intro/task_3/dashboard/src/Notifications.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './Notifications.css';
+import closeIcon from './close-icon.png';
+import { getLatestNotification } from './utils';
+
+const Notifications = () => {
+    const handleClick = () => {
+        console.log('Close button has been clicked');
+    }
+    return (
+        <>
+            <div style={{ position: 'relative' }} className='Notifications'>
+                <button aria-label='Close' onClick={handleClick}
+                    style={{ position: 'absolute', top: '0', right: '0', padding: '.3rem',  border: 'none', background: 'none', }}
+                >
+                        <img src={closeIcon} alt='Close Icon' style={{ height: '1.3rem', width: '1.3rem'}} />
+                </button>
+                <p>Here is the list of notifications</p>
+                <ul>
+                    <li data-priority='default'>New course available</li>
+                    <li data-priority='urgent'>New resume available</li>
+                    <li data-priority='urgent' dangerouslySetInnerHTML={{ __html: getLatestNotification() }}></li>
+                </ul>
+            </div>
+            </>
+    )
+}
+
+export default Notifications
