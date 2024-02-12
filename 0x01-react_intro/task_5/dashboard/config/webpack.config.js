0578b6e7c3af73908d363b2e21b9a87ec8e61c94
WIlliams <akanniwilliams@gmail.com>
2024-02-12 17:13:11 +0100

    Use one webpack config file

--- 0x01-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -4,6 +4,11 @@ const { title } = require("process");
 
 module.exports = {
     entry: "./src/index.js",
+    mode: "development",
+    output: {
+        filename: "bundle.js",    
+        path: path.resolve(__dirname, "dist")
+    }
     plugins: [
         new HtmlWebpackPlugin({
             title: "Dashboard",
