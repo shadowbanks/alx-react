e643864c80e58e16ad0aa6c74c333808517ed4b4
WIlliams <akanniwilliams@gmail.com>
2024-03-03 08:52:43 +0100

    New approach

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -3,12 +3,12 @@ import { Seq } from "immutable";
 const printBestStudents = (object) => {
   const format = (str) => str.charAt(0).toUpperCase() + str.slice(1);
   const result = Seq(object)
-    .filter((x) => x.score > 69)
-    .map((y) =>
-      y
-        .updateIn(["firstName"], format(y.get("firstName")))
-        .updateIn(["lastName"], format(y.get("lastName")))
-    )
+    .filter((student) => student.score > 69)
+    .map((student) => ({
+      ...student,
+      firstName: format(student.firstName),
+      lastName: format(student.lastName),
+    }))
     .toJS();
   console.log(result);
 };
