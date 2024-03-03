e4e43336e51d04f437235434b61919aa7d3b1b88
WIlliams <akanniwilliams@gmail.com>
2024-03-03 08:35:06 +0100

    Modify Approach

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -1,11 +1,14 @@
 import { Seq } from "immutable";
 
 const temp = (object) => {
-    const format = (str) => (str.charAt(0).toUpperCase() + str.slice(1));
-    Seq(object)
-    .filter(x => x.score > 69)
-    .map(y => 
-        y.updateIn(['firstName'], format(y.get('firstName')))
-        .updateIn(['lastName'], format(y.get('lastName')))
-    ).forEach(console.log(y.toJS()));
+  const format = (str) => str.charAt(0).toUpperCase() + str.slice(1);
+  const result = Seq(object)
+    .filter((x) => x.score > 69)
+    .map((y) =>
+      y
+        .updateIn(["firstName"], format(y.get("firstName")))
+        .updateIn(["lastName"], format(y.get("lastName")))
+    )
+    .toJS();
+  console.log(result);
 };
