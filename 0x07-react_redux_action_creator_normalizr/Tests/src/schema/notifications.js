973be235c862c02c612b0ea9714a60615dbf19f2
WIlliams <akanniwilliams@gmail.com>
2024-03-06 06:44:10 +0100

    Normalize a nested JSON

--- 0x07-react_redux_action_creator_normalizr/Tests/src/schema/notifications.js
+++ 0x07-react_redux_action_creator_normalizr/Tests/src/schema/notifications.js
@@ -1,8 +1,24 @@
-import notifications from '../../../notifications.json';
+import notificationData from '../../../notifications.json';
+import { normalize, schema } from 'normalizr'
+
+// Define users schema
+const user = new schema.Entity("users");
+
+// Define messages schema
+const message = new schema.Entity("messages", {}, {
+    idAttribute: 'guid',
+});
+
+// Define notifications schema
+const notification = new schema.Entity("notifications", {
+    author: user,
+    context: message,
+});
+
+export const normalizedData = normalize(notificationData, [notification]);
 
 const getAllNotificationsByUser = (userId) => {
-    // console.log(notifications);
-    return notifications.filter((notification) => notification.author.id === userId).map((notification) => notification.context);
+    return notificationData.filter((notification) => notification.author.id === userId).map((notification) => notification.context);
 };
 
 export default getAllNotificationsByUser;
---
+++ 0x07-react_redux_action_creator_normalizr/task_1/dashboard/src/BodySection/BodySectionWithMarginBottom.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import BodySection from "./BodySection";
+import propsTypes from 'prop-types';
+import { StyleSheet, css } from "aphrodite";
+
+class BodySectionWithMarginBottom extends Component {
+    render(){
+    return(
+        <div className={`bodySectionWithMargin ${css(style.bodySectionWithMargin)}`}>
+            <BodySection {...this.props}  />
+        </div>
+    )}
+}
+
+BodySectionWithMarginBottom.propsTypes = {
+    title: propsTypes.string.isRequired,
+    children: propsTypes.oneOfType([propsTypes.string, propsTypes.element])
+}
+
+const style = StyleSheet.create({
+    bodySectionWithMargin: {
+        marginBottom: "40px",
+    }    
+})
+export default BodySectionWithMarginBottom;
